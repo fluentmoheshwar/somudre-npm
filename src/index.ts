d034fad7aa82e33d3804bee73743641453d8c153
Moheshwar Amarnath Biswas <sayhi@moheshwar.com>
2026-06-23 19:40:48 +0600

    init commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const email: string = "wife@moheshwar.com";
+
+console.log("Hi, I'm Somudre Sarkar.");
+
+console.log(`\nYou can also reach me at: ${email}`);
+
+console.log(`\nThank you for visiting my profile!`);
